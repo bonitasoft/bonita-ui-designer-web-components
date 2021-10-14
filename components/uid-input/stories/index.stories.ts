fd18cb5be038da7d095c4b4cb2850c7691092da3
Jerome Cambon <jerome.p.cambon@gmail.com>
2021-10-14 16:43:57 +0200

    storybook update2

--- components/uid-input/stories/index.stories.ts
+++ components/uid-input/stories/index.stories.ts
@@ -8,6 +8,16 @@ export default {
     label: { control: 'text' },
     labelHidden: { control: 'boolean' },
     value: { control: 'text' },
+    required: { control: 'boolean' },
+    minLength: { control: 'number' },
+    maxLength: { control: 'number'},
+    labelPosition: {
+      options: ['top', 'left'],
+      control: 'radio'
+    },
+    labelWidth: { control: 'number' },
+    min: { control: 'number' },
+    max: { control: 'number' }
   },
 };
 
@@ -21,18 +31,40 @@ interface ArgTypes {
   label?: string;
   labelHidden?: boolean;
   value?: string;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  labelPosition?: string;
+  labelWidth?: number;
+  min?: number;
+  max?: number;
   slot?: TemplateResult;
 }
 
 const Template: Story<ArgTypes> = ({
-  label = 'my label', labelHidden = false,
+  label = "default",
+  labelHidden = false,
   value = "abc",
+  required = true,
+  minLength = 3,
+  maxLength = 10,
+  labelPosition = 'top',
+  labelWidth,
   slot,
+  min,
+  max
 }: ArgTypes) => html`
   <uid-input
     label=${label}
-    label-hidden=${labelHidden}
     value=${value}
+    min-length=${minLength}
+    max-length=${maxLength}
+    label-position=${labelPosition}
+    label-width=${labelWidth}
+    min=${min}
+    max=${max}
+    ${getLabelHidden(labelHidden)}
+    ${getRequired(required)}
   >
     ${slot}
   </uid-input>
@@ -42,7 +74,7 @@ export const Regular = Template.bind({});
 
 export const CustomLabel = Template.bind({});
 CustomLabel.args = {
-  label: 'My custom label',
+  label: 'My custom label'
 };
 
 export const CustomValue = Template.bind({});
@@ -57,3 +89,18 @@ SlottedContent.args = {
 SlottedContent.argTypes = {
   slot: { table: { disable: true } },
 };
+
+function getLabelHidden(labelHidden: boolean) {
+  if (!labelHidden) {
+    return html``;
+  }
+  return html`label-hidden`;
+}
+
+function getRequired(required: boolean) {
+  if (!required) {
+    return html``;
+  }
+  return html`required`;
+}
+
